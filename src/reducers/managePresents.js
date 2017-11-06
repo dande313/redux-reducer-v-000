2c7bdc0ec06043b6e6763d2e17bb6ef3c4d0aaa6
Allen Anderson <dande313@gmail.com>
2017-11-06 00:42:13 +0000

    automatically backed up by learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,5 +1,7 @@
 export function managePresents(state, action){
   switch(action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: ++state.numberOfPresents}
     default:
       return state
   }
