0b7ad4c2d8727bb27aef4f2aeb6847154ee3cacf
Allen Anderson <dande313@gmail.com>
2017-11-06 00:10:13 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,8 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND'
+      return Object.assign({}, state, {friends: [...state.friends, action.friend]})
+    case 'REMOVE_FRIEND'
+      return Object.assign({}, state, {friends: [...state.friends, action.friend]})
+  }
 }
